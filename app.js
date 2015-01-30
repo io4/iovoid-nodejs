8c0d75f73ee8442e41f9fde1088a21a017f2b28d
io <io@none.nul>
2015-01-30 14:24:34 -0300

    Remove blackhole

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var php = require("php");
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -23,7 +22,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use("/blackhole", php.cgi("/blackhole")); 
 
 app.use('/', routes);
 app.use('/users', users);
