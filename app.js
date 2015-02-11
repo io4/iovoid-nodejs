7d4767f654914ee58427ec3171682c5c410dc84c
io <io@none.nul>
2015-02-11 20:22:19 -0300

    Add beta auth sys

--- app.js
+++ app.js
@@ -4,6 +4,42 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passwordless = require('passwordless');
+var MongoStore = require('passwordless-mongostore-bcrypt-node');
+var email = require('nodejs');
+
+//begin of paswordless setup
+
+var smtpServer  = email.server.connect({
+   user:    fiegllucas@gmail.com, 
+   password: Electronica, 
+   host:    smtp.gmail.com, 
+   ssl:     true
+});
+
+ 
+var mongoURI = 'mongodb://admin:admin@ds041831.mongolab.com:41831/nodejs';
+passwordless.init(new MongoStore(mongoURI));
+
+passwordless.addDelivery(
+    function(tokenToSend, uidToSend, recipient, callback) {
+        var host = 'iovoid.tk/auth';
+        smtpServer.send({
+            text:    'Hello!\nAccess your account here: https://' 
+            + host + '?token=' + tokenToSend + '&uid=' 
+            + encodeURIComponent(uidToSend), 
+            from:    yourEmail, 
+            to:      recipient,
+            subject: 'Token for ' + host
+        }, function(err, message) { 
+            if(err) {
+                console.log(err);
+            }
+            callback(err);
+        });
+});
+
+//end of passwordless setup
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -22,6 +58,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/auth', passwordless.sessionSupport());
+app.use('/auth', passwordless.acceptToken({ successRedirect: '/'}));
+
+/* GET login screen. */
+router.get('/login', function(req, res) {
+   res.render('login');
+});
+
+/* POST login details. */
+router.post('/sendtoken', 
+    passwordless.requestToken(
+        // Turn the email address into an user ID
+        function(user, delivery, callback) {
+            // usually you would want something like:
+            User.find({email: user}, callback(ret) {
+               if(ret)
+                  callback(null, ret.id)
+               else
+                  callback(null, null)
+          })
+          // but you could also do the following 
+          // if you want to allow anyone:
+          // callback(null, user);
+        }),
+    function(req, res) {
+       // success!
+          res.render('sent');
+});
 
 app.use('/', routes);
 app.use('/users', users);
