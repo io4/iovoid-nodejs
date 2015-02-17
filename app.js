e09e9f9dc9bd454ac75ae979a8aac810134396b4
io <io@none.nul>
2015-02-17 17:48:51 -0300

    Fix...?

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(function(err, req, res, next) {
     });
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
 
   var host = server.address().address
   var port = server.address().port
