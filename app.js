d39e742aa07dbf18a1f9e487428b9aaae8ca36a8
io <io@none.nul>
2015-02-25 23:59:20 -0300

    Fix...?

--- app.js
+++ app.js
@@ -5,11 +5,19 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
-var users = require('./routes/users');
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var users = require('./routes/users')
 
 var app = express();
+
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+//var app = express();
 
 
 // view engine setup
@@ -33,22 +41,78 @@ app.use(function(req, res, next) {
   next();
 });
 
-io.on('connection', function(socket){
-  console.log('A user connected');
-});
+// Chatroom
 
-io.on('connection', function(socket){
-  console.log('A user connected');
-  socket.on('disconnect', function(){
-    console.log('User disconnected');
+// usernames which are currently connected to the chat
+var usernames = {};
+var numUsers = 0;
+
+io.on('connection', function (socket) {
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    // we store the username in the socket session for this client
+    socket.username = username;
+    // add the client's username to the global list
+    if(usernames[username]==username){
+ socket.emit('error in username', {
+      reason: "Nick Already taken"
+    });
+} else {
+    usernames[username] = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+}
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    // remove the username from global usernames list
+    if (addedUser) {
+      delete usernames[socket.username];
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
   });
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
 
 app.use('/', routes);
 app.use('/users', users);
@@ -86,8 +150,5 @@ if (app.get('env') === 'development') {
       res.status(500);
      res.render('500.jade', {title:'500: Internal Server Error', error: error});
   });
-
-http.listen(process.env.PORT || 3000, function(){
-});
 
 module.exports = app;
